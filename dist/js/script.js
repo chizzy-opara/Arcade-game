91bb4e201448b0c5085dd190b67ab3f5ebad5100
Chizzy-Opara <chizitereopara@gmail.com>
2022-09-30 12:49:00 +0100

    Code update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,30 +1,69 @@
-const player = document.querySelector("player");
+// const player = document.querySelector("player");
+class Player {
+    constructor(id, row = 5, col = 2) {
+        this.element = document.getElementById(id);
+        this.row = row;
+        this.col = col;
+        this.element.setAttribute('data-row', this.row);
+        this.element.setAttribute('data-col', this.col);
+        this.element.setAttribute('data-player-active', 'yes');
+    }
 
+    moveLeft() {
+        if(this.col === 0) {
+            // console.warn('you have reached the bottom')
+            return;
+        }
+
+        this.col = this.col - 1;
+        this.element.setAttribute('data-col', this.col);
+    }
+    moveUp() {
+        if(this.row === 0) {
+            // console.warn('you have reached the bottom')
+            return;
+        }
+
+        this.row = this.row - 1;
+        this.element.setAttribute('data-row', this.row);
+    }
+    moveRight() {
+        if(this.col === 4) {
+            // console.warn('you have reached the bottom')
+            return;
+        }
+
+        this.col = this.col + 1;
+        this.element.setAttribute('data-col', this.col);
+    }
+    moveDown() {
+        if(this.row === 5) {
+            // console.warn('you have reached the bottom')
+            return;
+        }
+
+        this.row = this.row + 1;
+        this.element.setAttribute('data-row', this.row);
+    }
+    
+
+
+}
+
+const charBoy = new Player('player');
 document.onkeydown = function (event) {
     switch (event.keyCode) {
        case 37:
-            moveLeft();
+            charBoy.moveLeft();
             break;
        case 38:
-            moveUp();
+            charBoy.moveUp() 
             break;
        case 39:
-            moveRight();
+            charBoy.moveRight();
             break;
        case 40:
-            moveDown();
+            charBoy.moveDown();
             break;
     }
  };
-function moveLeft() {
-   player.style.left += "5px";
-}
-function moveRight(){
-   player.style.right += "5px";
-}
-function moveDown(){
-   player.style.bottom += "5px";
-}
-function moveUp(){
-   player.style.top += "5px";
-};
