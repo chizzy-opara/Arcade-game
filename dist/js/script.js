7d4b075595ee46690dad4b392202f98c7112024f
Chizzy-Opara <chizitereopara@gmail.com>
2022-10-06 12:35:32 +0100

    wip

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+bugMove();
 // const player = document.querySelector("player");
 class Player {
     constructor(id, row = 5, col = 2) {
@@ -67,3 +68,57 @@ document.onkeydown = function (event) {
             break;
     }
  };
+
+ function bugMove() {
+    let id = null;
+    const elem = document.getElementById("bug");
+    let pos = 0;
+    move();
+    function move() {
+        if (pos == 100) {
+            pos =0;
+        } else {
+            pos = pos + .5;
+            elem.style.left = pos + '%';
+        }
+        window.requestAnimationFrame(move);
+    }
+
+    // Enemy.prototype.update = function (dt) {
+    //     this.x += this.speed * dt;
+    
+    //     if (this.x > 510) {
+    //         this.x = -50;
+    //         this.speed = 100 + Math.floor(Math.random() * 222);
+    //     };
+      
+    //     if (player.x < this.x + 80 &&
+    //         player.x + 80 > this.x &&
+    //         player.y < this.y + 60 &&
+    //         60 + player.y > this.y) {
+    //         player.x = 202;
+    //         player.y = 405;
+    //     };
+    // };
+
+
+    // clearInterval(id);
+    // id = setInterval(frame, 25);
+    // function frame() {
+    //     if (pos > 100) {
+    //         elem.style.display = 'none';
+    //     } else if(pos < 0) {
+    //         elem.style.display = 'none';
+    //     } else {
+    //         elem.style.display = 'block';
+    //     }
+    //   if (pos == 100) {
+    //     pos = -20;
+    //     elem.style.left = "-20%";
+    //   } else {
+    //     pos++; 
+    //     // elem.style.top = 100 + "px";
+    //     elem.style.left = pos + "%"; 
+    //   }
+    // }
+  }
